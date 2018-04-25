96ce24af04edc584397961c5a8883097f5c442a2
ele_hopper <elenacerezo83@gmail.com>
2018-04-25 10:15:00 +0200

    solved problems

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.second-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
@@ -10,14 +10,16 @@ function setDate() {
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + 90;
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;;
     minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 
 }
 
+
 setInterval(setDate, 1000);
+setDate();
